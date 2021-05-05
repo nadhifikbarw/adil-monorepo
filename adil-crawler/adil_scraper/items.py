ca2776a846cd1dfee39026881bde413f84c91f7f
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-05 17:28:26 +0700

    Remove Items and Middlewares component from scraper

--- adil-crawler/adil_scraper/items.py
+++ adil-crawler/adil_scraper/items.py
@@ -3,10 +3,4 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
-
-
-class Peraturan(scrapy.Item):
-    # define the fields for your item here like:
-    name = scrapy.Field()
-    url = scrapy.Field()
+# import scrapy
