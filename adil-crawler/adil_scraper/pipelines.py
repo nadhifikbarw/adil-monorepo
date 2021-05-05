787d207da7f98521f3110640bc5c6b85e2f03f09
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-05 13:52:44 +0700

    Initialize scrapy project for data collection

---
+++ adil-crawler/adil_scraper/pipelines.py
@@ -0,0 +1,10 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+# from adil_scraper.items import Peraturan
+# from itemadapter import ItemAdapter
+# from scrapy.exceptions import DropItem
