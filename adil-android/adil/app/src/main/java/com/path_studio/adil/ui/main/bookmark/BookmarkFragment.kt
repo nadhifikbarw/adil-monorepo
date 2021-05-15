d62fbde719014a13becbb8fa4bb869e4b1d8b0a5
Santoni Dyaz <tonisan1080p@gmail.com>
2021-05-15 15:32:13 +0700

    Generate Dummy Categories and Bookmarks

--- adil-android/adil/app/src/main/java/com/path_studio/adil/ui/main/bookmark/BookmarkFragment.kt
+++ adil-android/adil/app/src/main/java/com/path_studio/adil/ui/main/bookmark/BookmarkFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.path_studio.adil.R
 import com.path_studio.adil.databinding.FragmentBookmarkBinding
+import com.path_studio.adil.ui.main.category.CategoryFragment
 
 class BookmarkFragment : Fragment() {
 
@@ -22,4 +25,82 @@ class BookmarkFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+            val bookmarks = dummyData()
+            val bookmarkAdapter = BookmarkAdapter()
+            bookmarkAdapter.setBookmark(bookmarks)
+            with(binding.rvListBookmark) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = bookmarkAdapter
+            }
+        }
+    }
+
+    class Bookmark(
+        var bookmarkTitle : String = "",
+        var bookmarkDescription : String = "",
+        var bookmarkDitetapkan : String = "",
+        var bookmarkBerlaku : String = ""
+    )
+
+    //Dummy Data
+    private fun dummyData() : ArrayList<Bookmark> {
+        var items: ArrayList<Bookmark> = ArrayList()
+
+        items.add(
+            Bookmark(
+                "Peraturan Pemerintah Tahun 2020",
+                "Pelaksanaan undang-undang tentang cipta kerja sektor energi dan sumber daya mineral",
+                "Ditetapkan : 24 Februari 2020",
+                "Berlaku : 26 Februari 2020"
+            )
+        )
+        items.add(
+            Bookmark(
+                "Peraturan Pemerintah Tahun 2020",
+                "Pelaksanaan undang-undang tentang cipta kerja sektor energi dan sumber daya mineral",
+                "Ditetapkan : 24 Februari 2020",
+                "Berlaku : 26 Februari 2020"
+            )
+        )
+        items.add(
+            Bookmark(
+                "Peraturan Pemerintah Tahun 2020",
+                "Pelaksanaan undang-undang tentang cipta kerja sektor energi dan sumber daya mineral",
+                "Ditetapkan : 24 Februari 2020",
+                "Berlaku : 26 Februari 2020"
+            )
+        )
+        items.add(
+            Bookmark(
+                "Peraturan Pemerintah Tahun 2020",
+                "Pelaksanaan undang-undang tentang cipta kerja sektor energi dan sumber daya mineral",
+                "Ditetapkan : 24 Februari 2020",
+                "Berlaku : 26 Februari 2020"
+            )
+        )
+        items.add(
+            Bookmark(
+                "Peraturan Pemerintah Tahun 2020",
+                "Pelaksanaan undang-undang tentang cipta kerja sektor energi dan sumber daya mineral",
+                "Ditetapkan : 24 Februari 2020",
+                "Berlaku : 26 Februari 2020"
+            )
+        )
+        items.add(
+            Bookmark(
+                "Peraturan Pemerintah Tahun 2020",
+                "Pelaksanaan undang-undang tentang cipta kerja sektor energi dan sumber daya mineral",
+                "Ditetapkan : 24 Februari 2020",
+                "Berlaku : 26 Februari 2020"
+            )
+        )
+
+
+        return items
+    }
 }
