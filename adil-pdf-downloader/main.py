a4900139da988e96d5fba99d12fd015b3f868bfd
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-17 21:26:55 +0700

    Try adding exception handler and user-agent

--- adil-pdf-downloader/main.py
+++ adil-pdf-downloader/main.py
@@ -1,8 +1,10 @@
 from firebase_admin import credentials
 from firebase_admin import firestore
+from urllib.error import HTTPError
 from pathlib import Path
 import urllib.request
 import firebase_admin
+import urllib
 import os
 
 if __name__ == '__main__':
@@ -19,12 +21,24 @@ if __name__ == '__main__':
     # Setup output folder
     os.makedirs('docs', exist_ok=True)
 
+    # Set user agent
+    opener = urllib.request.build_opener()
+    opener.addheaders = [(
+        'User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
+    )]
+    urllib.request.install_opener(opener)
+
+    # Download all document
     docs = db.collection("legislation").stream()
 
     for doc in docs:
         filename = f"{doc.id}.0.pdf"
         document = doc.to_dict()['document'][0]
         url = document.replace(' ', '%20')
-        print(f"Downloading {filename}")
         if not dry_run:
-            urllib.request.urlretrieve(url, f"docs/{filename}")
+            try:
+                urllib.request.urlretrieve(url, f"docs/{filename}",)
+                print(f"Downloading {filename} OK")
+            except HTTPError as e:
+                print(f"Downloading {filename} FAIL {e.code} - {url}")
+
