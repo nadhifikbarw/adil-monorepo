f54acea6cb090c1f6beca0e9059265624edcd08e
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-20 19:25:06 +0700

    Create getSignedURL functions

--- adil-cloud-functions/functions/index.js
+++ adil-cloud-functions/functions/index.js
@@ -1,7 +1,12 @@
+const admin = require("firebase-admin");
+admin.initializeApp();
+
 const {rebuildIndex} = require("./rebuildIndex");
 const syncIndex = require("./syncIndex");
+const getSignedUrl = require("./getSignedUrl");
 
 exports.rebuildIndex = rebuildIndex;
 exports.handleCreate = syncIndex.handleCreate;
 exports.handleUpdate = syncIndex.handleUpdate;
 exports.handleDelete = syncIndex.handleDelete;
+exports.getSignedUrl = getSignedUrl.getSignedUrl;
