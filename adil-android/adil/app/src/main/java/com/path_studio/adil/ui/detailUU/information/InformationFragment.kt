37341f0bdf1ce8da5b2c976b10291a982634a5da
Santoni Dyaz <tonisan1080p@gmail.com>
2021-05-22 01:50:11 +0700

    default pdfviewer

--- adil-android/adil/app/src/main/java/com/path_studio/adil/ui/detailUU/information/InformationFragment.kt
+++ adil-android/adil/app/src/main/java/com/path_studio/adil/ui/detailUU/information/InformationFragment.kt
@@ -1,11 +1,15 @@
 package com.path_studio.adil.ui.detailUU.information
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.path_studio.adil.databinding.FragmentInformationBinding
+import com.path_studio.adil.ui.detailUU.DetailUUActivity
+import com.path_studio.adil.ui.main.MainActivity
+import com.path_studio.adil.ui.pdfView.PdfViewActivity
 
 class InformationFragment : Fragment() {
 
@@ -20,6 +24,16 @@ class InformationFragment : Fragment() {
         _binding = FragmentInformationBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(activity as DetailUUActivity, PdfViewActivity::class.java)
+            intent.putExtra(PdfViewActivity.EXTRA_ID,"11e44c4e281ce9d0947a313231323039")
+            startActivity(intent)
+        }
     }
 
 }
--- adil-android/adil/app/src/main/java/com/path_studio/adil/ui/pdfView/PdfViewActivity.kt
+++ adil-android/adil/app/src/main/java/com/path_studio/adil/ui/pdfView/PdfViewActivity.kt
@@ -1,16 +1,67 @@
 package com.path_studio.adil.ui.pdfView
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.github.barteksc.pdfviewer.util.FitPolicy
+import com.google.firebase.firestore.FirebaseFirestore
+import com.krishna.fileloader.FileLoader
+import com.krishna.fileloader.listener.FileRequestListener
+import com.krishna.fileloader.pojo.FileResponse
+import com.krishna.fileloader.request.FileLoadRequest
 import com.path_studio.adil.databinding.ActivityPdfViewBinding
+import java.io.File
+
 
 class PdfViewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPdfViewBinding
+
+    companion object{
+        val EXTRA_ID = "extra_id"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPdfViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val db = FirebaseFirestore.getInstance()
+
+        val extras = intent.extras
+        if(extras != null) {
+            val docId =extras.getString(EXTRA_ID)
+
+            val docRef = db.collection("legislation").document(docId.toString())
+            docRef.get()
+                .addOnSuccessListener { document ->
+                    if(document != null){
+
+                        //get data as array & choose the first
+                        val group = document["document"] as List<String>?
+                        val firstPdf = group?.elementAt(0)
+
+                        FileLoader.with(this)
+                            .load(firstPdf,false)
+                            .fromDirectory("PDFFile", FileLoader.DIR_INTERNAL)
+                            .asFile(object: FileRequestListener<File> {
+                                override fun onLoad(request: FileLoadRequest?, response: FileResponse<File>?){
+
+                                    val pdfFile = response!!.body
+                                    binding.pdfView.fromFile(pdfFile).pageFitPolicy(FitPolicy.WIDTH).load()
+
+                                }
+                                override fun onError(request: FileLoadRequest?, t: Throwable?) {
+                                    true
+                                }
+                            })
+                    }else{
+                        //Array "document" di Firestore kosong
+                    }
+                }.addOnFailureListener {
+                    //nothing for now
+                }
+
+        }
 
         //set back button listener
         binding.backButton.setOnClickListener {
