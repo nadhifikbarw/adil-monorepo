1a74f41648a781549db2fe6469d4db1cc2b94cfe
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-23 18:39:56 +0700

    Update json response data classes

--- adil-android/adil/app/src/main/java/com/path_studio/adil/data/source/remote/response/CategoryResponse.kt
+++ adil-android/adil/app/src/main/java/com/path_studio/adil/data/source/remote/response/CategoryResponse.kt
@@ -1,20 +1,16 @@
 package com.path_studio.adil.data.source.remote.response
 
-import com.google.gson.annotations.SerializedName
-
 data class CategoryResponse(
 
-    @field:SerializedName("count")
     val count: Int? = null,
 
-    @field:SerializedName("icon")
     val icon: String? = null,
 
     @field:JvmField
     val isFrontCategory: Boolean? = null,
 
-    @field:SerializedName("name")
-    val name: String? = null,
+    val name: String? = null
+
 ) {
     var id: String = ""
 }
