413755e259ffd8e4bc26103f2f520c51b7c9f269
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-27 01:41:36 +0700

    Create data class for queryResponse and fix serialization bug and implement queryLegislation and getTimeline methods

--- adil-android/adil/app/src/main/java/com/path_studio/adil/data/source/remote/RemoteDataSource.kt
+++ adil-android/adil/app/src/main/java/com/path_studio/adil/data/source/remote/RemoteDataSource.kt
@@ -2,15 +2,16 @@ package com.path_studio.adil.data.source.remote
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import com.google.android.gms.tasks.Task
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.path_studio.adil.data.source.remote.firestore.FirestoreConfig
-import com.path_studio.adil.data.source.remote.response.CategoryResponse
-import com.path_studio.adil.data.source.remote.response.LegislationResponse
+import com.path_studio.adil.data.source.remote.response.*
 
 class RemoteDataSource {
 
@@ -146,16 +147,48 @@ class RemoteDataSource {
             }
     }
 
-//    fun queryLegislation(query: String): Task<Any> {
-//        val data = "query" to query
-//
-//        return functions
-//            .getHttpsCallable("queryLegislation")
-//            .call(data)
-//            .continueWith { task ->
-//                val result = task.result?.data
-//                result
-//            }
-//    }
+    fun getTimeline(docId: String): Task<List<RelationshipItem?>> {
+        val data = hashMapOf(
+            "docId" to docId
+        )
 
+        return functions
+            .getHttpsCallable("getTimeline")
+            .call(data)
+            .continueWith { task ->
+                val results = task.result?.data as List<Object>
+
+                // Cast Map to JSON
+                val gson = Gson()
+                val json = gson.toJson(results)
+
+                // Type
+                val listType = object : TypeToken<List<RelationshipItem?>>() {}.type
+                val timeline = gson.fromJson<List<RelationshipItem?>>(json, listType)
+
+                timeline
+            }
+    }
+
+    fun queryLegislation(query: String): Task<List<QueryHitItem?>> {
+        val data = hashMapOf(
+            "query" to query
+        )
+
+        return functions
+            .getHttpsCallable("queryLegislation")
+            .call(data)
+            .continueWith { task ->
+                val resultMap = task.result?.data as Map<String,Any>
+
+                // Cast Map to JSON
+                val gson = Gson()
+                val json = gson.toJson(resultMap)
+
+                // Extract List of QueryHitItem
+                val queryResponse = gson.fromJson(json, QueryResponse::class.java)
+                val result = queryResponse.hits.hits
+                result
+            }
+    }
 }
