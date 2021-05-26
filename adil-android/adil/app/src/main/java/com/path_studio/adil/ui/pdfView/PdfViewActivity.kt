5d255e42ac99f897c57bffc3ae58083184b18ab3
nadhifikbarw <nadhifikbarw@gmail.com>
2021-05-27 01:42:10 +0700

    Cleanup PdfView

--- adil-android/adil/app/src/main/java/com/path_studio/adil/ui/pdfView/PdfViewActivity.kt
+++ adil-android/adil/app/src/main/java/com/path_studio/adil/ui/pdfView/PdfViewActivity.kt
@@ -2,11 +2,11 @@ package com.path_studio.adil.ui.pdfView
 
 import java.io.File
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.github.barteksc.pdfviewer.util.FitPolicy
 import com.krishna.fileloader.FileLoader
+import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
 import com.krishna.fileloader.pojo.FileResponse
+import com.github.barteksc.pdfviewer.util.FitPolicy
 import com.krishna.fileloader.request.FileLoadRequest
 import com.path_studio.adil.viewModel.ViewModelFactory
 import com.krishna.fileloader.listener.FileRequestListener
@@ -28,7 +28,6 @@ class PdfViewActivity : AppCompatActivity() {
         val factory = ViewModelFactory.getInstance(this)
         val viewModel = ViewModelProvider(this, factory)[PdfViewerViewModel::class.java]
 
-
         val extras = intent.extras
         if (extras != null) {
             val docLegisId = extras.getString(EXTRA_LEGISLATION_ID) as String
@@ -39,8 +38,7 @@ class PdfViewActivity : AppCompatActivity() {
                     showDocument(url)
                 }
 
-
-                //set back button listener
+                // Set back button listener
                 binding.backButton.setOnClickListener {
                     super.onBackPressed()
                 }
