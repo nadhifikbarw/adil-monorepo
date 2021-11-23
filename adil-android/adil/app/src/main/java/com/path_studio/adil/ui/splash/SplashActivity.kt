ee7db898b6ef7d9d1df038e2d86cb23c796fd416
Patricia Fiona <32255348+patriciafiona@users.noreply.github.com>
2021-11-23 16:14:40 +0700

    [Android] Disable Dark Mode
    Done:
    - Disable Dark Mode
    
    Must to be fixed:
    - Search function in search bar

--- adil-android/adil/app/src/main/java/com/path_studio/adil/ui/splash/SplashActivity.kt
+++ adil-android/adil/app/src/main/java/com/path_studio/adil/ui/splash/SplashActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.path_studio.adil.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //disable dark mode - just need once
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         Handler(mainLooper).postDelayed({
             val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
